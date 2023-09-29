036a680246eb3fd5db9d022cf759cfac623c52df
Paul Serratos <srp4343a@ens.fsi>
2023-09-29 09:47:17 +0200

    TP2

--- src/personnages/Gaulois.java
+++ src/personnages/Gaulois.java
@@ -14,8 +14,6 @@ public class Gaulois {
 		return nom;	
 	}
 	
-	//
-	
 	public void parler(String texte) {
 		System.out.println(prendreParole() + "<<" + texte + ">>");
 	}
@@ -25,7 +23,7 @@ public class Gaulois {
 	}
 
 	public void frapper(Romain romain) {
-		System.out.println(nom + "envoie un grand coup dans la mâchoire de " + romain.getNom());
+		System.out.println(nom + " envoie un grand coup dans la mâchoire de " + romain.getNom());
 		romain.recevoirCoup(force / 3);
 	}
 	
@@ -35,8 +33,13 @@ public class Gaulois {
 	}
 
 	public static void main(String[] args) {
+		asterix = new asterix();
 		
-	}
+	}	
+
+	System.out.println(asterix);
+
+}
 	
 	
 
--- src/personnages/Village.java
+++ src/personnages/Village.java
@@ -3,10 +3,12 @@ package personnages;
 public class Village {
 	private String nom;
 	private Chef chef;
+	private int nbVillageoisMaximum ;
 	
-	public Village(String nom, Chef chef) {
+	public Village(String nom, Chef chef, int nbVillageoisMaximum) {
 		this.nom = nom;
 		this.chef = chef;
+		this.nbVillageoisMaximum = nbVillageoisMaximum;
 	}
 	
 	public String getNom() {
@@ -16,5 +18,28 @@ public class Village {
 	public void setChef(Chef chef) {
 		this.chef = chef;
 	}
+	
+	// TP2 ---------------------------------------------------------------------------------------------
+	
+	private int nbVillageois ;
+	nbVillageois = 0 ;
+	
+	// création tableau villageois
+	String[] villageois = new String[nbVillageoisMaximum] ;
+	
+
+	// méthode ajouterHabitant
+	public int ajouterHabitant(String gaulois) {
+		villageois[nbVillageois] = gaulois;
+		nbVillageois += 1;
+	}
+	
+	// méthode trouverHabitant
+	public int trouverHabitant(int numero) {
+		return reference;
+	
+	}
+	
+	
 
 }
